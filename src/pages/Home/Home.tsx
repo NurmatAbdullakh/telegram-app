93edeeb84c2ceb46724a5a1e7b8bbda0abc8eb4c
Nurmat_Abdullaev <anstronnna@gmail.com>
2024-02-26 15:36:38 +0500

    test

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,24 @@
-import { useInitData, MainButton } from "@vkruglikov/react-telegram-web-app";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-nocheck
+import { MainButton } from "@vkruglikov/react-telegram-web-app";
+import useTelegram from "../../hooks/useTelegram";
 import { useEffect } from "react";
 
 const Home = () => {
-  const [InitDataUnsafe] = useInitData();
+  const tg = useTelegram();
 
   useEffect(() => {
-    console.log(InitDataUnsafe);
-  }, [InitDataUnsafe]);
+    console.log(tg);
+
+    if (tg) {
+      tg?.initDataUnsafe &&
+        console.log("====>>>", tg?.initDataUnsafe?.user?.first_name);
+    }
+  }, [tg]);
 
   return (
     <div>
-      {InitDataUnsafe?.user?.first_name}
+      {tg?.initDataUnsafe?.user?.first_name}
       <MainButton text="Click me" />
     </div>
   );
