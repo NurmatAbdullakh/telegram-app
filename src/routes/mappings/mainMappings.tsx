613f7c7edc5db618fd9eaf0c8fafb6f4ee06e02a
Nurmat_Abdullaev <anstronnna@gmail.com>
2024-02-26 08:42:49 +0500

    first commit

--- src/routes/mappings/mainMappings.tsx
+++ src/routes/mappings/mainMappings.tsx
@@ -1,11 +1,9 @@
 import { RouteProps } from "react-router-dom";
 import { mainPaths } from "../paths/mainPaths";
 
-
-
 export const mainMappings: RouteProps[] = [
-    {
-        path: mainPaths.Home,
-        element: <>Home</>,
-    },
-]
+  {
+    path: mainPaths.Home,
+    element: <>Home salom</>,
+  },
+];
