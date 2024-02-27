05fa8745123176ea989fdf0193bf54198acdd214
Nurmat_Abdullaev <anstronnna@gmail.com>
2024-02-27 16:59:07 +0500

    test

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 //@ts-nocheck
-import { MainButton } from "@vkruglikov/react-telegram-web-app";
 import useTelegram from "../../hooks/useTelegram";
 import { useEffect } from "react";
 
@@ -8,18 +7,70 @@ const Home = () => {
   const tg = useTelegram();
 
   useEffect(() => {
-    console.log(tg);
+    tg.ready()
 
-    if (tg) {
-      tg?.initDataUnsafe &&
-        console.log("====>>>", tg?.initDataUnsafe?.user?.first_name);
-    }
+     tg.expand();
+        tg.BackButton.show();
+        tg.MainButton.show();
+        tg.MainButton.text="I am a main button";
   }, [tg]);
+
+  const fullName = tg?.initDataUnsafe?.user?.first_name + " " + tg?.initDataUnsafe?.user?.last_name;
+  const userId = tg?.initDataUnsafe?.user?.id
+  
+  const onShowPopupClick=()=>{
+    tg.showPopup({
+      title: "Hello",
+      message: "World",
+      buttons: [
+        { type: "default", text: "OK", },
+        { type: "ok", text: "OK", },
+        { type: "cancel", text: "cancel", },
+      ]
+    },
+      (res) => {
+        console.log(res);
+      }
+    );
+    
+  }
+  const showAlert=()=>{
+    tg.showAlert("alert",
+    (res) => {
+      console.log(res);
+    });
+  }
+  const showConfirm=()=>{
+    tg.showConfirm("confirm",
+    (res) => {
+      console.log(res);
+      }
+    );
+  }
+  const showScanQrPopup=()=>{
+    tg.showScanQrPopup("scanQrPopup",
+    (res) => {
+      console.log(res);
+      }
+    );
+  }
+  const readTextFromClipboard = () => {
+    tg.showAlert(tg.readTextFromClipboard("readTextFromClipboard"))
+  }
 
   return (
     <div>
-      {tg?.initDataUnsafe?.user?.first_name}
-      <MainButton text="Click me" />
+      <div>
+      {fullName}
+      </div>
+      <div>
+      {userId}
+      </div>
+      <button onClick={onShowPopupClick} >showPopup</button>
+      <button onClick={showAlert} >showAlert</button>
+      <button onClick={showConfirm} >showConfirm</button>
+      <button onClick={showScanQrPopup} >showScanQrPopup</button>
+      <button onClick={readTextFromClipboard}>readTextFromClipboard</button>
     </div>
   );
 };
